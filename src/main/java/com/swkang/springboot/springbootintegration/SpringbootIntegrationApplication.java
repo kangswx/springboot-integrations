f6899be05498d55fcaa3534813f916b4b240256f
kangswx <996334837@qq.com>
2019-03-22 16:11:08 +0800

    开启异步任务的注解

--- src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
+++ src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
@@ -2,10 +2,12 @@ package com.swkang.springboot.springbootintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling        //扫描定时任务
+@EnableAsync             //开启异步任务
 public class SpringbootIntegrationApplication {
 
 	public static void main(String[] args) {
