ba24268104699e3537f8235cdbd069c0f06dd159
kangswx <996334837@qq.com>
2019-03-22 15:16:49 +0800

    修改通过工具类操作redis

--- src/main/java/com/swkang/springboot/springbootintegration/controller/RedisController.java
+++ src/main/java/com/swkang/springboot/springbootintegration/controller/RedisController.java
@@ -1,6 +1,9 @@
 package com.swkang.springboot.springbootintegration.controller;
 
 import com.swkang.springboot.springbootintegration.domain.JsonData;
+import com.swkang.springboot.springbootintegration.utils.RedisUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,24 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/redis")
 public class RedisController {
 
+    private static Logger logger = LoggerFactory.getLogger(RedisController.class);
+
     @Autowired
     private StringRedisTemplate redisTemplate;
+    @Autowired
+    private RedisUtils redisUtils;
 
     @GetMapping("add")
     private Object add(){
-        redisTemplate.opsForValue().set("name", "swkang");
+        //redisTemplate.opsForValue().set("name", "swkang");
+        redisUtils.set("name", "swkang");
         return JsonData.buildSuccess("ok");
     }
 
     @GetMapping("get")
     private Object get(String key){
-        String value = redisTemplate.opsForValue().get(key);
+        //String value = redisTemplate.opsForValue().get(key);
+        String value = redisUtils.get(key);
         return JsonData.buildSuccess(value);
     }
 
     @GetMapping("delete")
     private Object delete(String key){
-        redisTemplate.delete(key);
+        //redisTemplate.delete(key);
+        redisUtils.delete(key);
         return JsonData.buildSuccess("ok");
     }
 
