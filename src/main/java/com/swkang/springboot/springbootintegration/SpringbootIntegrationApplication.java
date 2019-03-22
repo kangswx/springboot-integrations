440ab0af1b5bb10fd8f5e28790aa5bb8e463f545
kangswx <996334837@qq.com>
2019-03-22 15:54:14 +0800

    新增扫描定时任务注解

--- src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
+++ src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
@@ -2,8 +2,10 @@ package com.swkang.springboot.springbootintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling        //扫描定时任务
 public class SpringbootIntegrationApplication {
 
 	public static void main(String[] args) {
