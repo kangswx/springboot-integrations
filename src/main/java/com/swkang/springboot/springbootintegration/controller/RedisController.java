e2fcba3e561c4b14785bdc6f925b037fa182fb8d
kangswx <996334837@qq.com>
2019-03-22 15:38:38 +0800

    修改保存对象的方法

--- src/main/java/com/swkang/springboot/springbootintegration/controller/RedisController.java
+++ src/main/java/com/swkang/springboot/springbootintegration/controller/RedisController.java
@@ -1,6 +1,8 @@
 package com.swkang.springboot.springbootintegration.controller;
 
 import com.swkang.springboot.springbootintegration.domain.JsonData;
+import com.swkang.springboot.springbootintegration.domain.User;
+import com.swkang.springboot.springbootintegration.utils.JsonUtils;
 import com.swkang.springboot.springbootintegration.utils.RedisUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1/redis")
@@ -40,6 +44,21 @@ public class RedisController {
         //redisTemplate.delete(key);
         redisUtils.delete(key);
         return JsonData.buildSuccess("ok");
+    }
+
+    @GetMapping("save_user")
+    public Object saveUser(){
+        User user = new User(15, "abc", "18945454545", new Date());
+        String userStr = JsonUtils.obj2String(user);
+        boolean success = redisUtils.set("base:user:11", userStr);
+        return JsonData.buildSuccess(success);
+    }
+
+    @GetMapping("find_user")
+    public Object findUser(){
+        String userStr = redisUtils.get("base:user:11");
+        User user = JsonUtils.string2Obj(userStr, User.class);
+        return JsonData.buildSuccess(user);
     }
 
 }
