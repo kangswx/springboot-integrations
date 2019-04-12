551fb611c6b78ec24d6e5bab8babb4a083da7541
kangswx <996334837@qq.com>
2019-04-12 21:53:29 +0800

    新增queue

--- src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
+++ src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
@@ -1,14 +1,25 @@
 package com.swkang.springboot.springbootintegration;
 
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.jms.Queue;
 
 @SpringBootApplication
 @EnableScheduling        //扫描定时任务
 @EnableAsync             //开启异步任务
+@EnableJms               //开启jms
 public class SpringbootIntegrationApplication {
+
+	@Bean                //将消息队列添加到spring容器，方便后续进行注入
+	public Queue queue(){
+		return new ActiveMQQueue("common.queue");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIntegrationApplication.class, args);
