5a4433e80e656c2d89761dc0539ca105e328468b
kangswx <996334837@qq.com>
2019-04-12 21:51:59 +0800

    新增JmsController

---
+++ src/main/java/com/swkang/springboot/springbootintegration/controller/JmsController.java
@@ -0,0 +1,34 @@
+package com.swkang.springboot.springbootintegration.controller;
+
+import com.swkang.springboot.springbootintegration.domain.JsonData;
+import com.swkang.springboot.springbootintegration.service.ProducerService;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.Destination;
+
+@RestController
+@RequestMapping("/api/v1/jms")
+public class JmsController {
+
+    @Autowired
+    private ProducerService producerService;
+
+    @GetMapping("order")
+    public Object order(String msg){
+        //生成新的消息队列
+        Destination destination = new ActiveMQQueue("order.queue");
+        producerService.sendMessage(destination, msg);
+        return JsonData.buildSuccess();
+    }
+
+    @GetMapping("common")
+    public Object common(String msg){
+        producerService.sendMessage(msg);
+        return JsonData.buildSuccess();
+    }
+
+}
