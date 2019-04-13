1adddea66645b01aa608b158e042e6070a777de5
kangswx <996334837@qq.com>
2019-04-13 10:25:04 +0800

    新增jmsListenerContainerTopic

--- src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
+++ src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 import javax.jms.Topic;
 
@@ -34,6 +37,19 @@ public class SpringbootIntegrationApplication {
 	@Bean
 	public Topic topic(){
 		return new ActiveMQTopic("video.topic");
+	}
+
+	/**
+	 * 可以同时支持点对点和发布订阅两种模式
+	 * @param activeMQConnectionFactory
+	 * @return
+	 */
+	@Bean
+	public JmsListenerContainerFactory<?> jmsListenerContainerTopic(ConnectionFactory activeMQConnectionFactory){
+		DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+		bean.setPubSubDomain(true);
+		bean.setConnectionFactory(activeMQConnectionFactory);
+		return bean;
 	}
 
 	public static void main(String[] args) {
