ef1fa215ef28acdca7ace7ed2e528772ec508e5a
kangswx <996334837@qq.com>
2019-04-13 10:04:42 +0800

    新增topic

--- src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
+++ src/main/java/com/swkang/springboot/springbootintegration/SpringbootIntegrationApplication.java
@@ -1,6 +1,7 @@
 package com.swkang.springboot.springbootintegration;
 
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.jms.Queue;
+import javax.jms.Topic;
 
 @SpringBootApplication
 @EnableScheduling        //扫描定时任务
@@ -16,9 +18,22 @@ import javax.jms.Queue;
 @EnableJms               //开启jms
 public class SpringbootIntegrationApplication {
 
+	/**
+	 * 添加默认消息队列
+	 * @return
+	 */
 	@Bean                //将消息队列添加到spring容器，方便后续进行注入
 	public Queue queue(){
 		return new ActiveMQQueue("common.queue");
+	}
+
+	/**
+	 * 添加默认主题
+	 * @return
+	 */
+	@Bean
+	public Topic topic(){
+		return new ActiveMQTopic("video.topic");
 	}
 
 	public static void main(String[] args) {
